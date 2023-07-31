287571e8b0333e5cddb7be6088918a9909369ce8
Atora <atora@giraffeduck.com>
2023-07-31 04:31:06 +0000

    fix(httplogger): use correct headers for logging info

--- src/middleware/HttpLoggerMiddleware.ts
+++ src/middleware/HttpLoggerMiddleware.ts
@@ -9,14 +9,15 @@ export class HttpLoggerMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl: url } = request
     const userAgent = request.get('user-agent') || ''
-    const realIp = request.get('x-forwarded-for') || ip
+    const realIp = request.get('cf-connecting-ip') || request.get('x-forwarded-for') || ip
+    const country - request.get('cf-ipcountry') || '??'
 
     response.on('close', () => {
       const { statusCode } = response
       const contentLength = response.get('content-length')
 
       this.logger.log(
-        `${method} ${url} ${statusCode} (${contentLength}) - ${userAgent} (${realIp})`,
+        `${method} ${url} ${statusCode} (${contentLength}) - ${userAgent} (${realIp}) [${country}]`,
       )
     })
 
